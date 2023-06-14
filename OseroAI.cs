4563593d3fcc697811f7eea0caf6a94fd0b12228
vinegarboy <necogusa398@gmail.com>
2023-06-14 14:09:56 +0900

    OseroAIを盤面の規格を守れば外でも使用可能に変更

--- OseroAI.cs
+++ OseroAI.cs
@@ -2,16 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Osero;
 
 namespace OseroAI{
     class AISystem{
-        //AIの脳内に存在する盤面情報
-        Bord bord = new Bord();
+        //盤面を保存する変数。0は何もおいてない状態。1が白で2が黒
+        public int[,] bord_data = new int[8,8];
 
         //自分の石の色
         int mycolor = 0;
 
+        private bool learnMode = false;
 
         //自分の石の情報をもらい取得する。
         public AISystem(int color){
@@ -20,7 +20,7 @@ namespace OseroAI{
 
         //盤面情報をセットする
         public void SetBord(int[,] bord_data){
-            bord.bord = bord_data;
+            this.bord_data = bord_data;
         }
 
         //自分の色を返す
@@ -32,7 +32,7 @@ namespace OseroAI{
         public int[] non_consider_put_stone(){
 
             //置ける場所を取得する。
-            List<int[]> choice_list = bord.can_put_list(mycolor);
+            List<int[]> choice_list = can_put_list();
 
             //おける場所がない場合、存在しない座標を返す。
             if(choice_list.Count == 0){
@@ -49,7 +49,7 @@ namespace OseroAI{
             int[] maxCoords = new int[] { -1, -1 };
 
             // 置ける場所を取得する。
-            List<int[]> choiceList = bord.can_put_list(mycolor);
+            List<int[]> choiceList = can_put_list();
 
             // おける場所がない場合、存在しない座標を返す。
             if (choiceList.Count == 0){
@@ -68,6 +68,79 @@ namespace OseroAI{
         }
 
 
+        //現在の盤面状況を保存するファイルを制作する関数。
+        public void learnBord(){
+            string path = @"./learnData/";
+            //現在の盤面データはファイル名にする。
+            path += now_bord_to_learnFileName();
+            if (!File.Exists(path)){
+                //ファイルが存在していない場合は盤面データを保存する用のファイルを作成。
+                File.CreateText(path);
+            }
+        }
+
+        //学習するかどうかの関数。
+        public void learnON(){
+            learnMode = true;
+            Directory.CreateDirectory("/learnData");
+        }
+
+        public void learnOFF(){
+            learnMode = false;
+        }
+
+        //石が置ける位置をList<int[]>で返す関数
+        private List<int[]> can_put_list() {
+            // 置ける位置を格納するリスト
+            List<int[]> put_list = new List<int[]>();
+
+            // 盤面を走査して、指定された色の石が置ける場所を探す
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    // 既に石が置かれている場合はスキップ
+                    if (bord_data[x, y] != 0) {
+                        continue;
+                    }
+
+                    // 置けるかどうかを判定するフラグ
+                    bool can_place = false;
+
+                    // 左右、上下、斜めの8方向について石を裏返す処理を行う
+                    for (int i = -1; i <= 1; i++) {
+                        for (int j = -1; j <= 1; j++) {
+                            // 自分自身の場合はスキップ
+                            if (i == 0 && j == 0) {
+                                continue;
+                            }
+                            // 現在の位置から指定方向に進んでいく
+                            int cur_x = x + i;
+                            int cur_y = y + j;
+
+                            // 次の位置が盤面内かつ相手の石である限り石を探し続ける
+                            while (cur_x >= 0 && cur_x < 8 && cur_y >= 0 && cur_y < 8 && bord_data[cur_x, cur_y] != 0 && bord_data[cur_x, cur_y] != mycolor) {
+                                // 次の位置に進む
+                                cur_x += i;
+                                cur_y += j;
+                            }
+
+                            // 最後の位置が自分の石である場合、石を置ける場所とする
+                            if (cur_x >= 0 && cur_x < 8 && cur_y >= 0 && cur_y < 8 && bord_data[cur_x, cur_y] == mycolor) {
+                                can_place = true;
+                            }
+                        }
+                    }
+
+                    // 石を置ける場合、座標をリストに追加
+                    if (can_place) {
+                        put_list.Add(new int[] {x, y});
+                    }
+                }
+            }
+
+            // 置ける場所を返す
+            return put_list;
+        }
+
         //ひっくり返せる石の数を数える。
         private int CountFlipStones(int x, int y, int color){
             //座標とかの入力値のチェック
@@ -76,7 +149,7 @@ namespace OseroAI{
             }
 
             //石がすでに配置されていないかをチェックする。
-            if (bord.bord[x,y] != 0){
+            if (bord_data[x,y] != 0){
                 return -1;
             }
 
@@ -98,14 +171,14 @@ namespace OseroAI{
                     int flipCount = 0;
 
                     //石を置ける限り走査を続ける
-                    while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && bord.bord[nx,ny] != 0 && bord.bord[nx,ny] != color){
+                    while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && bord_data[nx,ny] != 0 && bord_data[nx,ny] != color){
                         flipCount++;
                         nx += dx;
                         ny += dy;
                     }
 
                     //カウントがある場合は代入する。
-                    if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && bord.bord[nx,ny] == color && flipCount > 0){
+                    if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8 && bord_data[nx,ny] == color && flipCount > 0){
                         count += flipCount;
                     }
                 }
@@ -113,5 +186,15 @@ namespace OseroAI{
             return count;
         }
 
+        private String now_bord_to_learnFileName(){
+            String name = "";
+            //現在の盤面データをファイル名用に変換する。
+            for(int x = 0;x<8;x++){
+                for(int y = 0;y<8;y ++){
+                    name += Convert.ToString(bord_data[x,y]);
+                }
+            }
+            return name;
+        }
     }
 }
